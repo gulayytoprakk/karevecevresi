79e1704a60904a916439cda0d3ec28f470c8b35d
Gülay TOPRAK <140807385+gulayytoprakk@users.noreply.github.com>
2023-07-29 01:05:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int kenar, karealan, karecevre;
+        Console.WriteLine("karenin çevresini ve alanını bulma");
+        Console.WriteLine("karenin kenarını giriniz:");
+        kenar=Convert.ToInt32(Console.ReadLine());
+
+        karealan = kenar * kenar;
+        karecevre = kenar * 4;
+
+        Console.WriteLine("kare alanı=  "+karealan);
+        Console.WriteLine("kare cevresi= "+ karecevre);
+    }
+   
+}
